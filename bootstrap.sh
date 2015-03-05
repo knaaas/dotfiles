c6397a77413a8a67084cb55a5303361740b353f8
Anton Ingfors <Anton.Ingfors@gmail.com>
2015-03-05 23:37:37 +0100

    Exclude gnome-terminal-colors-solarized

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ git submodule sync
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "gnome-terminal-colors-solarized" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
