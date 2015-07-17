93dabc9552ffcc45f3df4971725d9934167343ee
Anton Ingfors <Anton.Ingfors@gmail.com>
2015-07-17 04:12:37 +0200

    windows bootstrap

---
+++ bootstrap.bat
@@ -0,0 +1,8 @@
+call git pull origin master
+call git submodule init
+call git submodule update 
+call git submodule sync 
+copy .vimrc C:%HOMEPATH% /Y
+copy .emacs C:%HOMEPATH% /Y
+copy .gitconfig C:\%HOMEPATH% /Y
+xcopy .vim C:%HOMEPATH%\vimfiles /E /I /Y
