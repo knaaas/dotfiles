2cd5f93deba4d95f55716ef654a130c4abc1d70f
Anton Ingfors <anton.ingfors@gmail.com>
2019-10-31 22:25:25 +0100

    Removing unused config

--- bootstrap.bat
+++ bootstrap.bat
@@ -2,7 +2,5 @@ call git pull origin master
 call git submodule init
 call git submodule update 
 call git submodule sync 
-copy .vimrc C:%HOMEPATH%\.gvimrc /Y
-copy .emacs C:%HOMEPATH% /Y
-copy .gitconfig C:\%HOMEPATH% /Y
+copy /Y .vimrc C:%HOMEPATH%\
 xcopy .vim C:%HOMEPATH%\vimfiles /E /I /Y
