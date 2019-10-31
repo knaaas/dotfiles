45a48b8af4468ccfabbae2896d089682ebe39d48
Anton Ingfors <anton.ingfors@gmail.com>
2019-10-31 22:33:13 +0100

    Make output look a little bit better

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,6 +1,8 @@
+@echo off
 call git pull origin master
 call git submodule init
 call git submodule update 
 call git submodule sync 
+echo "Installing VIM Config files"
 copy /Y .vimrc C:%HOMEPATH%\
 xcopy .vim C:%HOMEPATH%\vimfiles /E /I /Y
